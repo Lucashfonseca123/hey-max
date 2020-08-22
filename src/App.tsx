1c391af026708ade9f1e724c4df2c6cedc98da7a
lucas.fonseca <lucas.fonseca@trizy.com.br>
2020-08-21 23:32:53 -0300

    Corrigido splash

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {StatusBar, SafeAreaView, Text} from 'react-native';
 import {Provider} from 'react-redux';
 import {store, persistor} from './store/Store';
 import {PersistGate} from 'redux-persist/integration/react';
-// import SplashScreen from 'react-native-splash-screen';
+import SplashScreen from 'react-native-splash-screen';
 
 import RootNavigator from './navigation/RootNavigator';
 
@@ -14,7 +14,7 @@ const App = () => {
   useEffect(() => {
     Text.defaultProps = Text.defaultProps || {};
     Text.defaultProps.allowFontScaling = false;
-    // SplashScreen.hide();
+    SplashScreen.hide();
   }, []);
 
   return (
